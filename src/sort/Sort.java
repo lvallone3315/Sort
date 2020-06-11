4fd2d852e05bca761b6e8ca948ccd69425b4cb71
lvall <lvall@LAPTOP-K6J9MVMK>
2020-06-11 18:22:49 -0400

    Generate random array of MAX_SIZE integers Print array Sort array using bubble sort Print output array Capture Stats - array size, # reads, # swaps

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -5,6 +5,8 @@
  */
 package sort;
 
+import java.util.Random;
+
 /**
  *
  * @author lvall
@@ -17,6 +19,45 @@ public class Sort {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Sort Me!");
+        
+        // Stat tracking
+        int ifCount = 0;
+        int swapCount = 0;
+        
+        // Generate random set of integers
+        final int MAX_SIZE = 20;
+        int[] inputArray = new int[MAX_SIZE];
+        
+        Random rand = new Random();
+                    
+        for (int loop = 0; loop < MAX_SIZE; loop++) {
+            inputArray[loop] = rand.nextInt(1000);
+        }
+        
+        // Print input array
+        for (int loop = 0; loop < MAX_SIZE; loop++) {
+            System.out.printf("%d ", inputArray[loop]);
+        }
+        System.out.println();
+        
+        // Sort array in place
+        int temp = 0;
+        for (int outerLoop = 0; outerLoop < MAX_SIZE-1; outerLoop++){
+            for (int innerLoop = 0; innerLoop < MAX_SIZE-outerLoop-1; innerLoop++) {
+                if (inputArray[innerLoop] > inputArray[innerLoop+1]) {
+                    temp = inputArray[innerLoop];
+                    inputArray[innerLoop] = inputArray[innerLoop+1];
+                    inputArray[innerLoop+1] = temp;
+                    swapCount++;
+                }
+                ifCount++;
+            }
+            for (int loop = 0; loop < MAX_SIZE; loop++) {
+                System.out.printf("%d ", inputArray[loop]);
+            }
+            System.out.println();
+        }
+        System.out.printf("Array size: %d\t# reads: %d\t# writes %d\n", MAX_SIZE, ifCount, swapCount);
     }
     
 }
