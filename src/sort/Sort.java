5953ea1f4b6ff23bd98fdebc2413270c7121d9cc
lvall <lvall@LAPTOP-K6J9MVMK>
2020-06-11 19:42:15 -0400

    Added version information Cleaned up comments - still no Java Doc comments Cleaned up array name -> sortArray    (prev inputArray, but sorting in place, input = output array)

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -1,11 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sort Project
+ *   Bubble sort of randomly generated array of integers (basic)
+ * 
+ * Coded all in one main() routine
+ *   Generate randomly set of MAX_SIZE integers (using random.nextInt())
+ *   Print input array to be sorted
+ *   Sort array using bubblesort
+ *     Print each iteration of bubblesort
+ *   Print stats
+ *
+ * Add version string
  */
 package sort;
 
 import java.util.Random;
+
 
 /**
  *
@@ -13,30 +22,34 @@ import java.util.Random;
  */
 public class Sort {
 
+    final static String VERSION = "Sort version 0.1";
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.printf("%s\n\n", VERSION);
         System.out.println("Sort Me!");
         
         // Stat tracking
+        //   ifCount = reads
+        //   swapCount = writes
         int ifCount = 0;
         int swapCount = 0;
         
         // Generate random set of integers
         final int MAX_SIZE = 20;
-        int[] inputArray = new int[MAX_SIZE];
+        int[] sortArray = new int[MAX_SIZE];
         
         Random rand = new Random();
                     
         for (int loop = 0; loop < MAX_SIZE; loop++) {
-            inputArray[loop] = rand.nextInt(1000);
+            sortArray[loop] = rand.nextInt(1000);
         }
         
         // Print input array
         for (int loop = 0; loop < MAX_SIZE; loop++) {
-            System.out.printf("%d ", inputArray[loop]);
+            System.out.printf("%d ", sortArray[loop]);
         }
         System.out.printf("\n\n");
         
@@ -44,19 +57,23 @@ public class Sort {
         int temp = 0;
         for (int outerLoop = 0; outerLoop < MAX_SIZE-1; outerLoop++){
             for (int innerLoop = 0; innerLoop < MAX_SIZE-outerLoop-1; innerLoop++) {
-                if (inputArray[innerLoop] > inputArray[innerLoop+1]) {
-                    temp = inputArray[innerLoop];
-                    inputArray[innerLoop] = inputArray[innerLoop+1];
-                    inputArray[innerLoop+1] = temp;
-                    swapCount++;
+                if (sortArray[innerLoop] > sortArray[innerLoop+1]) {
+                      // swap elements
+                    temp = sortArray[innerLoop];
+                    sortArray[innerLoop] = sortArray[innerLoop+1];
+                    sortArray[innerLoop+1] = temp;
+                    swapCount++;  // write count
                 }
-                ifCount++;
+                ifCount++;  // read count
             }
+            // print array after this iteration - one element "bubbles" down
             for (int loop = 0; loop < MAX_SIZE; loop++) {
-                System.out.printf("%d ", inputArray[loop]);
+                System.out.printf("%d ", sortArray[loop]);
             }
             System.out.println();
         }
+        
+        // Print stats
         System.out.printf("Array size: %d\t# reads: %d\t# writes %d\n", MAX_SIZE, ifCount, swapCount);
     }
     
